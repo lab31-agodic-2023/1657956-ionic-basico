b51a297fd05d41eeb9166a0e56a72076f75ce653
JIvan013 <seraphyn4@gmail.com>
2023-08-31 17:06:15 -0600

    first commit

---
+++ operaciones.js
@@ -0,0 +1,22 @@
+function botonclick() {
+        var articulo= document.getElementById("articulo")
+        var precio= document.getElementById("precio")
+        var x =articulo.value +"    $" + precio.value;
+        var divCr=document.createElement("div");
+        divCr.textContent=x
+        document.getElementById("artprecio").appendChild(divCr);
+        
+        document.getElementById("preciototal").textContent=document.getElementById("preciototal").textContent*1+precio.value*1;
+        articulo.value="";
+        precio.value="";
+    }
+function suma(){
+
+
+
+
+
+}
+
+
+
