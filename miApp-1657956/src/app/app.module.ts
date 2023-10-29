b8b5371b38d09ec5bf11fcf2f5fd411c5dc6afeb
JIvan013 <seraphyn4@gmail.com>
2023-10-29 14:18:04 -0600

    Practica 7

--- miApp-1657956/src/app/app.module.ts
+++ miApp-1657956/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
 
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,ListaAlumnosComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
